9288e28d7deddeebcf0ca51fb32e6d16d3fb0e7a
ronaldperaza <ronaldperaza460@gmail.com>
2021-04-04 20:49:39 -0400

    task app

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'crud'
+
+) or die(mysqli_error($mysqli));
+
+
+
+
+?>
---
+++ edit.php
@@ -0,0 +1,53 @@
+<?php
+include("db.php");
+$title = '';
+$description= '';
+
+//al darle boton editar se ejecuta, mysqli_query para traer los datos
+if  (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $query = "SELECT * FROM task WHERE id=$id";
+  $result = mysqli_query($conn, $query);
+  //si existe resultado comprobar cuantas fils tiene resultado
+  if (mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_array($result);
+    $title = $row['title'];
+    $description = $row['description'];
+  }
+}
+
+if (isset($_POST['update'])) {
+  $id = $_GET['id'];
+  $title= $_POST['title'];
+  $description = $_POST['description'];
+
+  $query = "UPDATE task set title = '$title', description = '$description' WHERE id=$id";
+  mysqli_query($conn, $query);
+  $_SESSION['message'] = 'Task Updated Successfully';
+  $_SESSION['message_type'] = 'warning';
+  header('Location: index.php');
+}
+
+?>
+<?php include('includes/header.php'); ?>
+<h1 class="display-4 text-center">Update Task</h1>
+<div class="container p-4">
+  <div class="row">
+    <div class="col-md-4 mx-auto">
+      <div class="card card-body">
+      <form action="edit.php?id=<?php echo $_GET['id']; ?>" method="POST">
+        <div class="form-group">
+          <input name="title" type="text" class="form-control" value="<?php echo $title; ?>" placeholder="Update Title">
+        </div>
+        <div class="form-group">
+        <textarea name="description" class="form-control mt-3 mb-3" cols="30" rows="10"><?php echo $description;?></textarea>
+        </div>
+        <button class="btn-success" name="update">
+          Actualizar
+        </button>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>
